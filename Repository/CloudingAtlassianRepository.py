ddbf8628b482ca2d61f71fe04d0f89e538daba5b
Mike Burns <mike@7ft10.com>
2023-06-09 20:35:13 +1000

    New Systems

--- Repository/CloudingAtlassianRepository.py
+++ Repository/CloudingAtlassianRepository.py
@@ -20,15 +20,30 @@ globals()['Personas'] = cPersonas()
 #@markdown # Internal Systems
 class cInternalSystems():
   def __init__(self):
-    self.Disc:SevenftNode = SevenftNode.LoadFromYaml(baseUrl + 'Repository/Internal%20Systems/Disc.yaml')
+    self.ActiveDirectory:SevenftNode = SevenftNode.LoadFromYaml(baseUrl + 'Repository/Internal%20Systems/ActiveDirectory.yaml')
     self.Bamboo:SevenftNode = SevenftNode.LoadFromYaml(baseUrl + 'Repository/Internal%20Systems/Bamboo.yaml')
+    self.BitBucket:SevenftNode = SevenftNode.LoadFromYaml(baseUrl + 'Repository/Internal%20Systems/BitBucket.yaml')
+    self.Citrix:SevenftNode = SevenftNode.LoadFromYaml(baseUrl + 'Repository/Internal%20Systems/Citrix.yaml')
     self.Confluence:SevenftNode = SevenftNode.LoadFromYaml(baseUrl + 'Repository/Internal%20Systems/Confluence.yaml')
+    self.ConfluenceNonProd:SevenftNode = SevenftNode.LoadFromYaml(baseUrl + 'Repository/Internal%20Systems/ConfluenceNonProd.yaml')
+    self.Disc:SevenftNode = SevenftNode.LoadFromYaml(baseUrl + 'Repository/Internal%20Systems/Disc.yaml')
+    self.GitLab:SevenftNode = SevenftNode.LoadFromYaml(baseUrl + 'Repository/Internal%20Systems/GitLab.yaml')
+    self.GlobalProtect:SevenftNode = SevenftNode.LoadFromYaml(baseUrl + 'Repository/Internal%20Systems/GlobalProtect.yaml')
+    self.Intranet:SevenftNode = SevenftNode.LoadFromYaml(baseUrl + 'Repository/Internal%20Systems/Intranet.yaml')
+    self.Jenkins:SevenftNode = SevenftNode.LoadFromYaml(baseUrl + 'Repository/Internal%20Systems/Jenkins.yaml')
+    self.Jira:SevenftNode = SevenftNode.LoadFromYaml(baseUrl + 'Repository/Internal%20Systems/Jira.yaml')
+    self.JiraNonProd:SevenftNode = SevenftNode.LoadFromYaml(baseUrl + 'Repository/Internal%20Systems/JiraNonProd.yaml')
+    self.Okta:SevenftNode = SevenftNode.LoadFromYaml(baseUrl + 'Repository/Internal%20Systems/Okta.yaml')
+    self.PowerBi:SevenftNode = SevenftNode.LoadFromYaml(baseUrl + 'Repository/Internal%20Systems/PowerBi.yaml')
+    self.ServiceNow:SevenftNode = SevenftNode.LoadFromYaml(baseUrl + 'Repository/Internal%20Systems/ServiceNow.yaml')
 
 globals()['InternalSystems'] = cInternalSystems()
 
 #@markdown # External Systems
 class cExternalSystems():
   def __init__(self):
+    self.ConfluenceCloud:SevenftNode = SevenftNode.LoadFromYaml(baseUrl + 'Repository/External%20Systems/ConfluenceCloud.yaml')
+    self.JiraCloud:SevenftNode = SevenftNode.LoadFromYaml(baseUrl + 'Repository/External%20Systems/JiraCloud.yaml')
     self.NewRelic:SevenftNode = SevenftNode.LoadFromYaml(baseUrl + 'Repository/External%20Systems/New%20Relic.yaml')
 
 globals()['ExternalSystems'] = cExternalSystems()
